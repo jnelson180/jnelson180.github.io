e65039ad9f3c1d93113686e13eb6608ea2930f83
Jake <jnelson180@gmail.com>
2016-09-18 17:16:31 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $("#projectcontainer").hide();
+    $("#contactcontainer").hide();
+  
+  $("#introNav").click(function() {    
+    $("#projectcontainer").hide( "fast", function() {$("#contactcontainer").hide( "fast", function() {$("#introcontainer").show("fast", function() {});});});
+    
+    
+  });
+  
+    $("#portfolioNav").click(function() {
+    $("#introcontainer").hide( "fast", function() {$("#contactcontainer").hide( "fast", function() {$("#projectcontainer").show("fast", function() {});});});
+
+  });
+  
+    $("#contactNav").click(function() {
+    $("#projectcontainer").hide( "fast", function() {$("#introcontainer").hide( "fast", function() {$("#contactcontainer").show("fast", function() {});});});
+    
+  });
+
+  $("li").click(function() {
+  $("li").each(function() {
+      $(this).removeClass("active");
+      $(this).removeClass("darkText");
+    }); 
+    $(this).addClass("active");
+    $(this).addClass("darkText");
+  });
+
+});
