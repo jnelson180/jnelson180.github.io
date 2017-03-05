376e71e5b21868f04f35ae0099a0a3b53d5e59c2
Jake Nelson <jnelson180@gmail.com>
2017-03-04 18:16:20 -0600

    Updated some minor bugs in css spacing

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,42 @@
 $(document).ready(function() {
     $("#projectcontainer").hide();
     $("#contactcontainer").hide();
-  
-  $("#introNav").click(function() {    
+
+  $("#introNav").click(function() {
     $("#projectcontainer").hide( "slow", function() {$("#contactcontainer").hide( "slow", function() {$("#introcontainer").show("slow", function() {});});});
-    
-    
+
+
   });
-  
+
     $("#portfolioNav").click(function() {
     $("#introcontainer").hide( "slow", function() {$("#contactcontainer").hide( "slow", function() {$("#projectcontainer").show("slow", function() {});});});
 
   });
-  
+
     $("#contactNav").click(function() {
     $("#projectcontainer").hide( "slow", function() {$("#introcontainer").hide( "slow", function() {$("#contactcontainer").show("slow", function() {});});});
-    
+
   });
 
   $("li").click(function() {
   $("li").each(function() {
       $(this).removeClass("active");
       $(this).removeClass("darkText");
-    }); 
+    });
     $(this).addClass("active");
     $(this).addClass("darkText");
+  });
+
+  $( "#contactButton" ).click(function() {
+    console.log("contactButton clicked");
+    $("#projects").hide();
+    $("#contact").show();
+  });
+
+  $("#projectsButton").click(function() {
+    console.log("projectsButton clicked");
+    $("#contact").hide();
+    $("#projects").show();
   });
 
 });
